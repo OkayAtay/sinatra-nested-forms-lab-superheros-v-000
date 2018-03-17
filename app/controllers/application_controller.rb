fd46a37f79bb8b004255804965a63bc1d87da786
Araba Taylor <araba.taylor20@gmail.com>
2018-03-17 17:36:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params[:team])
 
-      params[:team][:hero]
+      params[:team][:hero]. each do |details|
+      end
       erb :team
     end
 
